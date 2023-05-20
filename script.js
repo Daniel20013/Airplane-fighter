48e01cfc2f787090608cd7f02b6000ce2329e920
Daniel20013 <126924076+Daniel20013@users.noreply.github.com>
2023-05-20 19:56:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
+const plane = document.querySelector(".plane");
+
 function obstaclesAppear() {
     const containerObstacles = document.querySelector(".containerObstacles");
-    const obstacles = document.querySelector(".obstacles");
     const img = document.createElement("img");
-    img.src="assets/airplane_u2708_icon_256x256.png";
+    img.src = "assets/airplane_u2708_icon_256x256.png";
     const clone = img.cloneNode(true);
     clone.classList.add("obstacles");
     clone.style.left = Math.floor((76 - 1 + 1) * Math.random()) + "%";
     clone.style.display = "block";
     containerObstacles.appendChild(clone);
+  
+    setTimeout(function() {
+        clone.classList.add("down");
+        
+        setTimeout(function() {
+            clone.remove();
+            checkCollision(clone);
+        }, 5000);
+    }, 100);
 }
+
+
+  
 
 function pad(value) {
     return value.toString().padStart(2, "0");
@@ -29,8 +42,8 @@ function startTimer() {
         }
         let time = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
         document.getElementById("timer").textContent = time;
-        obstaclesAppear();
     }, 1000);
+    setInterval(obstaclesAppear, 1500);
 }
 
 function startGame() {
@@ -39,24 +52,23 @@ function startGame() {
     timer.style.display = "block";
     let start = document.getElementById("start");
     start.style.display = "none";
-    const plane = document.querySelector(".plane");
     plane.style.display = "block";
     let moveDistance = 52;
     document.addEventListener('keydown', function(event) {
         let = currentPosition = parseInt(plane.style.left || "0");
         if (event.key === "ArrowRight") {
-            moveDistance += 10;
-            if (moveDistance < 100) {
+            moveDistance += 5;
+            if (moveDistance < 95) {
                 plane.style.left = moveDistance + "%";
             } else {
-                moveDistance -= 10;
+                moveDistance -= 5;
             }
         } else if (event.key === "ArrowLeft") {
-            moveDistance -= 10;
+            moveDistance -= 5;
             if (moveDistance >= 0) {
                 plane.style.left = moveDistance + "%";
             } else {
-                moveDistance += 10;
+                moveDistance += 5;
             }
         }
     });
